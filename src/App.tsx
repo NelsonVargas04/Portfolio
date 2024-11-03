35b4bb9822ca678a8fef08100f9cb0a0dbef9b6b
NelsonVargas15 <var.nelson15@gmail.com>
2024-11-03 14:20:46 -0300

    feat: I will create a new color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import WorkExperience from './components/WorkExperience';
@@ -17,9 +17,9 @@ function AppContent() {
   };
 
   return (
-    <div className={`min-h-screen ${darkMode ? 'bg-black text-gray-100' : 'bg-white text-gray-900'} transition-colors duration-300`}>
+    <div className={`min-h-screen ${darkMode ? 'bg-[#0D1321] text-[#F0EBD8]' : 'bg-[#edf6f9] text-[#0D1321]'} transition-colors duration-300`}>
       <div className="fixed top-4 right-4 z-50">
-        <div className="flex items-center bg-gray-800 rounded-full px-4 py-3 shadow-lg">
+        <div className={`flex items-center  rounded-full px-4 py-3 shadow-lg" ${darkMode ? 'bg-[#1D2D44]' : 'bg-[#748CAB] '} `} >
           <div className="flex items-center">
             <img 
               src="https://flagcdn.com/w40/us.png"
@@ -27,9 +27,9 @@ function AppContent() {
               className={`w-8 h-5 ${language === 'en' ? 'opacity-100' : 'opacity-50'}`}
             />
             <div className="mx-2 w-12 h-6 relative cursor-pointer" onClick={toggleLanguage}>
-              <div className="w-12 h-6 bg-gray-600 rounded-full"></div>
+              <div className="w-12 h-6 bg-[#3E5C76] rounded-full"></div>
               <div 
-                className={`absolute top-0.5 left-0.5 w-5 h-5 bg-white rounded-full transition-transform duration-300 ${
+                className={`absolute top-0.5 left-0.5 w-5 h-5 bg-[#F0EBD8] rounded-full transition-transform duration-300 ${
                   language === 'es' ? 'translate-x-6' : 'translate-x-0'
                 }`}
               ></div>
@@ -40,34 +40,36 @@ function AppContent() {
               className={`w-8 h-5 ${language === 'es' ? 'opacity-100' : 'opacity-50'}`}
             />
           </div>
-          <div className="mx-2 w-px h-6 bg-gray-600"></div>
+          <div className="mx-2 w-px h-6 bg-[#748CAB]"></div>
           <div className="flex items-center">
-            <svg className={`w-8 h-8 ${darkMode ? 'opacity-50' : 'text-yellow-400'}`} fill="currentColor" viewBox="0 0 20 20">
+            <svg className={`w-8 h-8 ${darkMode ? 'opacity-50' : 'text-[#edf6f9]'}`} fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clipRule="evenodd" />
             </svg>
             <div className="mx-2 w-12 h-6 relative cursor-pointer" onClick={toggleDarkMode}>
-              <div className="w-12 h-6 bg-gray-600 rounded-full"></div>
+              <div className="w-12 h-6 bg-[#3E5C76] rounded-full"></div>
               <div 
-                className={`absolute top-0.5 left-0.5 w-5 h-5 bg-white rounded-full transition-transform duration-300 ${
+                className={`absolute top-0.5 left-0.5 w-5 h-5 bg-[#F0EBD8] rounded-full transition-transform duration-300 ${
                   darkMode ? 'translate-x-6' : 'translate-x-0'
                 }`}
               ></div>
             </div>
-            <svg className={`w-8 h-8 ${darkMode ? 'text-gray-300' : 'opacity-50'}`} fill="currentColor" viewBox="0 0 20 20">
+            <svg className={`w-8 h-8 ${darkMode ? 'text-[#edf6f9]' : 'opacity-50'}`} fill="currentColor" viewBox="0 0 20 20">
               <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
             </svg>
           </div>
         </div>
       </div>
-      <Header />
-      <main className={`max-w-7xl mx-auto px-4 py-4 ${darkMode ? 'bg-black' : 'bg-white'}`}>
-        <About />
-        <WorkExperience />
-        <Projects />
-        <Certificates />
-        <Technologies />
-        <Contact />
+      <div className={`max-w-7xl mx-auto px-4 py-4 ${darkMode ? 'bg-[#0D1321]' : 'bg-[#edf6f9]'}`}>
+      <Header darkMode ={darkMode}  />
+      <main >
+        <About  darkMode ={darkMode}/>
+        <WorkExperience darkMode ={darkMode} />
+        <Projects darkMode ={darkMode} />
+        <Certificates darkMode ={darkMode} />
+        <Technologies darkMode={darkMode} />
+        <Contact darkMode={darkMode} />
       </main>
+      </div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
 import { useLanguage } from '../contexts/LanguageContext';
 import { translations } from '../translations/translations';
 import { Linkedin, Github, Mail, FileText } from 'lucide-react';
 
-const Header: React.FC = () => {
+const Header: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
   const { language } = useLanguage();
   const t = translations[language];
 
   return (
-    <header className={`h-screen flex flex-col justify-center items-center bg-gradient-to-b from-gray-900 to-black text-white transition-colors duration-300`}>
+    <header className={`h-screen flex flex-col justify-center items-center ${darkMode ? ' text-[#edf6f9]' : 'text-[#0D1321]'} transition-colors duration-300`}>
       <div className="text-center">
-        <h1 className="text-9xl font-bold mb-4 animate-scaleIn">Nelson Vargas</h1>
-        <p className="text-4xl text-blue-400 mb-8 animate-slideIn">{t.role}</p> {/* Increased subtitle size */}
+        <h1 className={`text-9xl font-bold mb-4 animate-scaleIn ${darkMode ? 'text-[#edf6f9]' : 'text-[#1d2d44]'}`}>Nelson Vargas</h1>
+        <p className={`text-4xl mb-8 animate-slideIn ${darkMode ? 'text-[#00b4d8]' : 'text-[#0077b6]'}`}>{t.role}</p>
         <div className="flex justify-center space-x-6 animate-fadeIn">
-          <a href="https://www.linkedin.com/in/nelson-vargas-470893254/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-300 transition-colors">
+          <a href="https://www.linkedin.com/in/nelson-vargas-470893254/" target="_blank" rel="noopener noreferrer" className={`${darkMode ? 'text-[#edf6f9]' : 'text-[#1D2D44]'} hover:text-blue-300 transition-colors`}>
             <Linkedin className="w-10 h-10" />
           </a>
-          <a href="https://github.com/NelsonVargas04" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-300 transition-colors">
+          <a href="https://github.com/NelsonVargas04" target="_blank" rel="noopener noreferrer" className={`${darkMode ? 'text-[#edf6f9]' : 'text-[#1D2D44]'} hover:text-blue-300 transition-colors`}>
             <Github className="w-10 h-10" />
           </a>
-          <a href="mailto:varnelson15.@gmail.com" className="text-white hover:text-blue-300 transition-colors">
+          <a href="mailto:varnelson15.@gmail.com" className={`${darkMode ? 'text-[#edf6f9]' : 'text-[#1D2D44]'} hover:text-blue-300 transition-colors`}>
             <Mail className="w-10 h-10" />
           </a>
-          <a href="/Nelson_Vargas_CV.pdf" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-300 transition-colors">
+          <a href="/Nelson_Vargas_CV.pdf" target="_blank" rel="noopener noreferrer" className={`${darkMode ? 'text-[#edf6f9]' : 'text-[#1D2D44]'} hover:text-blue-300 transition-colors`}>
             <FileText className="w-10 h-10" />
           </a>
         </div>
