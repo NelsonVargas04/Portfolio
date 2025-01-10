52020bf00c51f1dfee91e08a1e8319c4919b16b1
Nelson Vargas <var.nelson@gmail.com>
2025-01-09 22:26:17 -0300

    feat: change

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -37,32 +37,32 @@ const projectsData: Project[] = [
     technologies: ["React", "Node.js", "Express", "MongoDB", "Stripe"],
   },
   {
-    title: "Task Management App",
+    title: "Password Assistant",
     description: {
-      es: "Una aplicación móvil de gestión de tareas desarrollada con React Native",
-      en: "A mobile task management app developed with React Native",
+      es: "Aplicación para generar y verificar contraseñas de manera segura.",
+      en: "An app to securely generate and check passwords.",
     },
     fullDescription: {
-      es: "Aplicación móvil para gestión de tareas con funciones de creación, edición, eliminación y categorización de tareas, así como notificaciones y sincronización en la nube.",
-      en: "Mobile app for task management with features for creating, editing, deleting, and categorizing tasks, as well as notifications and cloud synchronization.",
+      es: "Herramienta interactiva para evaluar la fortaleza de contraseñas y generar nuevas claves seguras, incluyendo contraseñas aleatorias, fáciles de recordar y PINs, con opciones personalizables.",
+      en: "Interactive tool to evaluate password strength and generate secure keys, including random, memorable passwords, and PINs, with customizable options.",
     },
     image:
-      "https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+      "src/components/img/generate.png",
     github: "https://github.com/nelsonvargas/task-management-app",
     technologies: ["React Native", "Redux", "Firebase"],
   },
   {
-    title: "Weather Forecast Dashboard",
+    title: "Pet Selector",
     description: {
-      es: "Un panel de pronóstico del tiempo utilizando Angular y la API de OpenWeatherMap",
-      en: "A weather forecast dashboard using Angular and OpenWeatherMap API",
+      es: "Interfaz interactiva para elegir tu mascota favorita entre diferentes categorías.",
+      en: "Interactive interface to choose your favorite pet from different categories.",
     },
     fullDescription: {
-      es: "Dashboard interactivo que muestra pronósticos detallados del tiempo para múltiples ubicaciones, con gráficos, mapas y alertas meteorológicas.",
-      en: "Interactive dashboard displaying detailed weather forecasts for multiple locations, featuring charts, maps, and weather alerts.",
+      es: "Aplicación intuitiva que permite seleccionar tu mascota preferida entre perros, gatos y peces. Incluye opciones detalladas y visuales para facilitar la elección.",
+      en: "Intuitive application that lets you select your preferred pet among dogs, cats, and fish. It includes detailed and visual options to make the choice easier.",
     },
     image:
-      "https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+      "src/components/img/mascotas.png",
     github: "https://github.com/nelsonvargas/weather-forecast-dashboard",
     live: "https://weather-forecast-dashboard-demo.netlify.app",
     technologies: ["Angular", "TypeScript", "RxJS", "OpenWeatherMap API"],
@@ -81,9 +81,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
       {/* Título de la sección */}
       <div className="relative inline-flex items-center gap-4 mb-8 bg-gradient-to-r from-purple-500/20 to-transparent p-6 rounded-lg">
         <h2
-          className={`text-4xl font-semibold ${
-            darkMode ? "text-[#00b4d8]" : "text-[#1D2D44]"
-          }`}
+          className={`text-4xl font-semibold ${darkMode ? "text-[#00b4d8]" : "text-[#1D2D44]"
+            }`}
         >
           {t.projectsTitle}
         </h2>
@@ -94,9 +93,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
         {projectsData.map((project, index) => (
           <div
             key={index}
-            className={`rounded-lg shadow-md overflow-hidden transition-all duration-300 hover:scale-105 cursor-pointer ${
-              darkMode ? "bg-gray-800" : "bg-gray-200"
-            }`}
+            className={`rounded-lg shadow-md overflow-hidden transition-all duration-300 hover:scale-105 cursor-pointer ${darkMode ? "bg-gray-800" : "bg-gray-200"
+              }`}
             onClick={() => setSelectedProject(index)}
           >
             <img
@@ -106,16 +104,14 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
             />
             <div className="p-4">
               <h3
-                className={`text-2xl font-semibold mb-2 ${
-                  darkMode ? "text-[#edf6f9]" : "text-[#0D1321]"
-                }`}
+                className={`text-2xl font-semibold mb-2 ${darkMode ? "text-[#edf6f9]" : "text-[#0D1321]"
+                  }`}
               >
                 {project.title}
               </h3>
               <p
-                className={`text-base mb-4 ${
-                  darkMode ? "text-gray-400" : "text-gray-700"
-                }`}
+                className={`text-base mb-4 ${darkMode ? "text-gray-400" : "text-gray-700"
+                  }`}
                 style={{
                   maxHeight: "50px",
                   overflow: "hidden",
@@ -131,9 +127,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
                 {project.technologies.map((tech, techIndex) => (
                   <span
                     key={techIndex}
-                    className={`px-3 py-2 rounded-full text-sm ${
-                      darkMode ? "bg-blue-700 text-white" : "bg-blue-300 text-blue-800"
-                    }`}
+                    className={`px-3 py-2 rounded-full text-sm ${darkMode ? "bg-blue-700 text-white" : "bg-blue-300 text-blue-800"
+                      }`}
                   >
                     {tech}
                   </span>
@@ -145,9 +140,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
                     href={project.github}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={`flex items-center ${
-                      darkMode ? "text-blue-400 hover:text-blue-300" : "text-blue-800 hover:text-blue-600"
-                    }`}
+                    className={`flex items-center ${darkMode ? "text-blue-400 hover:text-blue-300" : "text-blue-800 hover:text-blue-600"
+                      }`}
                   >
                     <Github className="w-5 h-5 mr-1" />
                     GitHub
@@ -157,9 +151,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
                       href={project.live}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className={`flex items-center ${
-                        darkMode ? "text-green-400 hover:text-green-300" : "text-green-800 hover:text-green-600"
-                      }`}
+                      className={`flex items-center ${darkMode ? "text-green-400 hover:text-green-300" : "text-green-800 hover:text-green-600"
+                        }`}
                     >
                       <Globe className="w-5 h-5 mr-1" />
                       Live Demo
@@ -176,9 +169,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
       {selectedProject !== null && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
           <div
-            className={`rounded-lg max-w-6xl w-full p-12 relative ${
-              darkMode ? "bg-gray-800" : "bg-gray-200"
-            }`}
+            className={`rounded-lg max-w-6xl w-full p-12 relative ${darkMode ? "bg-gray-800" : "bg-gray-200"
+              }`}
           >
             <button
               onClick={closeModal}
@@ -187,9 +179,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
               <X className="w-6 h-6" />
             </button>
             <h3
-              className={`text-3xl font-semibold mb-4 ${
-                darkMode ? "text-[#edf6f9]" : "text-[#0D1321]"
-              }`}
+              className={`text-3xl font-semibold mb-4 ${darkMode ? "text-[#edf6f9]" : "text-[#0D1321]"
+                }`}
             >
               {projectsData[selectedProject].title}
             </h3>
@@ -199,9 +190,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
               className="w-full h-[400px] object-cover rounded-lg mb-4"
             />
             <p
-              className={`text-lg leading-relaxed mb-6 ${
-                darkMode ? "text-gray-400" : "text-gray-700"
-              }`}
+              className={`text-lg leading-relaxed mb-6 ${darkMode ? "text-gray-400" : "text-gray-700"
+                }`}
             >
               {projectsData[selectedProject].fullDescription[language]}
             </p>
@@ -209,9 +199,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
               {projectsData[selectedProject].technologies.map((tech, index) => (
                 <span
                   key={index}
-                  className={`px-4 py-2 rounded-full text-base ${
-                    darkMode ? "bg-blue-700 text-white" : "bg-blue-300 text-blue-800"
-                  }`}
+                  className={`px-4 py-2 rounded-full text-base ${darkMode ? "bg-blue-700 text-white" : "bg-blue-300 text-blue-800"
+                    }`}
                 >
                   {tech}
                 </span>
@@ -222,9 +211,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
                 href={projectsData[selectedProject].github}
                 target="_blank"
                 rel="noopener noreferrer"
-                className={`flex items-center text-lg ${
-                  darkMode ? "text-blue-400 hover:text-blue-300" : "text-blue-800 hover:text-blue-600"
-                }`}
+                className={`flex items-center text-lg ${darkMode ? "text-blue-400 hover:text-blue-300" : "text-blue-800 hover:text-blue-600"
+                  }`}
               >
                 <Github className="w-6 h-6 mr-1" />
                 GitHub
@@ -234,9 +222,8 @@ const Projects: React.FC<{ darkMode: boolean }> = ({ darkMode }) => {
                   href={projectsData[selectedProject].live}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className={`flex items-center text-lg ${
-                    darkMode ? "text-green-400 hover:text-green-300" : "text-green-800 hover:text-green-600"
-                  }`}
+                  className={`flex items-center text-lg ${darkMode ? "text-green-400 hover:text-green-300" : "text-green-800 hover:text-green-600"
+                    }`}
                 >
                   <Globe className="w-6 h-6 mr-1" />
                   Live Demo
